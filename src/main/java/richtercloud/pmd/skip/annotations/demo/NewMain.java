9ad393a7dd6563a2e7c211d7be7652095a089240
Karl-Philipp Richter <krichter722@aol.de>
2017-07-27 17:32:10 +0200

    initial commit

---
+++ src/main/java/richtercloud/pmd/skip/annotations/demo/NewMain.java
@@ -0,0 +1,24 @@
+package richtercloud.pmd.skip.annotations.demo;
+
+public class NewMain {
+
+    @SuppressWarnings("PMD.UnusedFormalParameter")
+    private void method1(Object arg1) {
+        System.out.println("method1");
+    }
+
+    @SuppressWarnings("PMD.UnusedFormalParameter")
+    private void method2(Object arg1) {
+        System.out.println("method2");
+    }
+
+    @SuppressWarnings("PMD.UnusedFormalParameter")
+    private void method3(Object arg1) {
+        System.out.println("method3");
+    }
+
+    @SuppressWarnings("PMD.UnusedFormalParameter")
+    private void method4(Object arg1) {
+        System.out.println("method4");
+    }
+}
